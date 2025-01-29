05653ecefb2cb48dbe373bab37e00c5ea0c62d2d
MR <mr@MRs-MacBook-Pro.local>
2025-01-30 01:43:00 +0600

    newd

--- container/home-page/footer/Footer.tsx
+++ container/home-page/footer/Footer.tsx
@@ -18,10 +18,15 @@ export default function Footer() {
        sm:flex-col sm:space-y-5 xm:flex-col text-[20px]"
       >
         <div>
-          <h2 className="uppercase link-flash cursor-pointer">LinkDIN</h2>
+          <h2
+            className="uppercase link-flash cursor-pointer"
+            onClick={() => window.open("https://www.linkedin.com/in/mahbub-rahim-004016159/", "_blank")}
+          >
+            LinkDIN
+          </h2>
         </div>
         <div className="">
-          <h2 >@2024</h2>
+          <h2>@2025</h2>
         </div>
 
         <div className=" ">
